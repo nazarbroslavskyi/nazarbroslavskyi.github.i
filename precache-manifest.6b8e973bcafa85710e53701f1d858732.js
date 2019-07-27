194f589b5f06a7bdc60424358d03a5524433cb69
nazya <nazarbroslaw>
2019-07-27 18:44:37 +0300

    Updates

---
+++ precache-manifest.6b8e973bcafa85710e53701f1d858732.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ff86daa4d16d8ec1f8554f148bf894",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3440db92fd553e529dfc",
+    "url": "/todo-list/static/css/main.d7a19141.chunk.css"
+  },
+  {
+    "revision": "344e611bf12fbf234486",
+    "url": "/todo-list/static/js/2.e736923e.chunk.js"
+  },
+  {
+    "revision": "3440db92fd553e529dfc",
+    "url": "/todo-list/static/js/main.f55b8137.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
